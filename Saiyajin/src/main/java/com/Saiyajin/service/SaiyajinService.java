b6444ece17b611e3e03427d59a77cdb0b455a520
jaxJC <seg.valdebenito@duocuc.cl>
2025-04-28 15:54:49 -0400

    Guardando cambios locales antes del pull

--- Saiyajin/src/main/java/com/Saiyajin/service/SaiyajinService.java
+++ Saiyajin/src/main/java/com/Saiyajin/service/SaiyajinService.java
@@ -1,5 +1,31 @@
 package com.Saiyajin.service;
 
-public class SaiyajinService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-}
+
+public class SaiyajinService {
+    @Autowired
+    private SaiyajinRepository saiyajinRepository;
+
+     public List<Saiyajin> getSaiyajins() {
+        return saiyajinRepository.obtenerSaiyajin();
+    }
+
+     public Saiyajin saveSaiyajin(Saiyajin saiyajin) {
+        saiyajinRepository.guardar(saiyajin);
+    }
+
+    public Saiyajin getSaiyajinId(int id) {
+        return saiyajinRepository.buscarPorId(id);
+    }
+    public Saiyajin updateSaiyajin(Saiyajin saiyajin) {
+        return saiyajinRepository.actualizar(saiyajin);
+    }
+    public void deleteSaiyajin(int id) {
+        saiyajinRepository.eliminar(id);
+        return "saiyajin eliminado";
+    }
+
+
+
