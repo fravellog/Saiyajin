96cb83d8e5d5333623bcca0446209e487caf1792
Valentiwew <bascunanbenjamin759@gmail.com>
2025-04-28 16:39:59 -0400

    fix: 🧊com.Saiyajin.controller;

--- src/main/java/com/Saiyajin/controller/SaiyajinController.java
+++ src/main/java/com/Saiyajin/controller/SaiyajinController.java
@@ -1,8 +1,8 @@
-package com.Saiyajin.Saiyajin.controller;
+package com.Saiyajin.controller;
 
 /* Importamos las clases (model - service) */
-import com.Saiyajin.Saiyajin.model.Saiyajin; /* Clase Saiyajin */
-import com.Saiyajin.Saiyajin.service.SaiyajinService; /* Clase Servicio */
+import com.Saiyajin.model.Saiyajin; /* Clase Saiyajin */
+import com.Saiyajin.service.SaiyajinService; /* Clase Servicio */
 
 /* Importamos Anotaciones necesarias de Spring */
 import org.springframework.beans.factory.annotation.Autowired; /* Para importar dependencias automaticamente  */
